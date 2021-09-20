67b04c6544bd8014e73aa364dfdd647c36ea4539
SSHSEDANG4 <89133643+SSHSEDANG4@users.noreply.github.com>
2021-09-21 05:44:24 +0700

    Update install.sh

--- akses-main/install.sh
+++ akses-main/install.sh
@@ -4,9 +4,9 @@ echo "==INSTALL AKSES IP BY ENDKA=="
 #INSTALL AKSES
 cd
 cd /usr/bin
-wget -O addip "https://raw.githubusercontent.com/lesta-1/akses/main/addip.sh"
-wget -O delip "https://raw.githubusercontent.com/lesta-1/akses/main/delip.sh"
-wget -O xp-ip "https://raw.githubusercontent.com/lesta-1/akses/main/xp-ip.sh"
+wget -O addip "https://raw.githubusercontent.com/SSHSEDANG4/akses-ip-manager/main/akses-main/addip.sh"
+wget -O delip "https://raw.githubusercontent.com/SSHSEDANG4/akses-ip-manager/main/akses-main/delip.sh"
+wget -O xp-ip "https://raw.githubusercontent.com/SSHSEDANG4/akses-ip-manager/main/akses-main/xp-ip.sh"
 
 #PERMISSION
 cd
