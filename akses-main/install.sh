1f20e467d4a2a6a920c1c66b7973d70eec7e70f4
SSHSEDANG4 <89133643+SSHSEDANG4@users.noreply.github.com>
2021-09-21 08:00:20 +0700

    Update install.sh

--- akses-main/install.sh
+++ akses-main/install.sh
@@ -7,12 +7,14 @@ cd /usr/bin
 wget -O addip "https://raw.githubusercontent.com/SSHSEDANG4/akses-ip-manager/main/akses-main/addip.sh"
 wget -O delip "https://raw.githubusercontent.com/SSHSEDANG4/akses-ip-manager/main/akses-main/delip.sh"
 wget -O xp-ip "https://raw.githubusercontent.com/SSHSEDANG4/akses-ip-manager/main/akses-main/xp-ip.sh"
+wget -O im "https://raw.githubusercontent.com/SSHSEDANG4/akses-ip-manager/main/akses-main/im.sh"
 
 #PERMISSION
 cd
 chmod +x /usr/bin/addip
 chmod +x /usr/bin/delip
 chmod +x /usr/bin/xp-ip
+chmod +x /usr/bin/im
 
 cd /home
 mkdr listip
